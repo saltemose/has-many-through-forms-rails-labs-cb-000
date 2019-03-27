fdfae235e6d582ce23c6bcdf9322cbdd89279820
Sean Altemose <sean@seanaltemose.com>
2019-03-27 19:17:23 +0000

    Automatically backed up by Learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.find_or_create_by(comment_params)
+    comment = Comment.create(comment_params)
     redirect_to comment.post
   end
 
